96089a0f2b054b32ac3cf4c0b936dd293df17696
Emil Kuhejda <emil.kuhejda@gmail.com>
2022-01-18 13:18:11 +0100

    Set up Serilog and Autofac.

---
+++ src/web/MessageQueue.Host/Configuration/ApplicationModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using AutofacSerilogIntegration;
+
+namespace MessageQueue.Host.Configuration
+{
+    public class ApplicationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            RegisterModules(builder);
+            RegisterServices(builder);
+            ConfigureFilters(builder);
+            ConfigureMappings(builder);
+        }
+
+        private void RegisterModules(ContainerBuilder builder)
+        {
+        }
+
+        private void RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterLogger();
+        }
+
+        private void ConfigureFilters(ContainerBuilder builder)
+        {
+        }
+
+        private void ConfigureMappings(ContainerBuilder builder)
+        {
+        }
+    }
+}
--- src/web/MessageQueue.Host/Program.cs
+++ src/web/MessageQueue.Host/Program.cs
@@ -1,5 +1,11 @@
+using System;
+using System.IO;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 using MessageQueueHost = Microsoft.Extensions.Hosting.Host;
 
 namespace MessageQueue.Host
@@ -8,14 +14,52 @@ namespace MessageQueue.Host
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .ReadFrom.Configuration(configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File($"Logs/logs.txt",
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}]|[{SourceContext}] {Message:lj}{NewLine}{Exception}",
+                    fileSizeLimitBytes: 104857600,
+                    rollingInterval: RollingInterval.Month,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+
+            try
+            {
+                Log.ForContext<Program>().Information("Starting application up");
+
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.ForContext<Program>().Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             MessageQueueHost.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
